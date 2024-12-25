223c9591ee7ca8c21f46cf213f27b313db1b0fdc
rabbitmaster-sys <rigerphin@gmail.com>
2024-12-25 06:55:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
-import solver.solver as solver
+import solver
 
 app = Flask(__name__)
 
 @app.route('/solve')
 async def async_view():
-    response = await solver("https://modrinth.com/auth/sign-up","0x4AAAAAAAHWfmKCm7cUG869");
+    response = await solver.solver("https://modrinth.com/auth/sign-up","0x4AAAAAAAHWfmKCm7cUG869");
     return jsonify(response)
 
 if __name__ == '__main__':
